49b20f5a7e9b9a42861c13852c7fffb45abd8cdd
Michal Vlcek <mychalvlcek@gmail.com>
2017-01-01 21:28:31 +0100

    feat: add loggable mail sender decorator
    References: #1

---
+++ src/main/java/com/example/service/email/decorator/AbstractMailSenderDecorator.java
@@ -0,0 +1,73 @@
+package com.example.service.email.decorator;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+
+/**
+ * Base {@link JavaMailSender} decorator class.
+ * Class just wraps underlying (decorated) object.
+ * It is furthermore intended for usage with other decorators.
+ *
+ * @author Michal Vlcek <mychalvlcek@gmail.com>
+ * @see AsyncMailSender
+ * @see FileMailSender
+ * @see LoggableMailSender
+ */
+public abstract class AbstractMailSenderDecorator implements JavaMailSender {
+
+	/**
+	 * The {@link JavaMailSender} decorated instance.
+	 */
+	protected JavaMailSender mailSender;
+
+	public AbstractMailSenderDecorator(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+
+	@Override
+	public MimeMessage createMimeMessage() {
+		return this.mailSender.createMimeMessage();
+	}
+
+	@Override
+	public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+		return this.mailSender.createMimeMessage(contentStream);
+	}
+
+	@Override
+	public void send(MimeMessage mimeMessage) throws MailException {
+		this.mailSender.send(mimeMessage);
+	}
+
+	@Override
+	public void send(MimeMessage... mimeMessages) throws MailException {
+		this.mailSender.send(mimeMessages);
+	}
+
+	@Override
+	public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+		this.mailSender.send(mimeMessagePreparator);
+	}
+
+	@Override
+	public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+		this.mailSender.send(mimeMessagePreparators);
+	}
+
+	@Override
+	public void send(SimpleMailMessage simpleMessage) throws MailException {
+		this.mailSender.send(simpleMessage);
+	}
+
+	@Override
+	public void send(SimpleMailMessage... simpleMessages) throws MailException {
+		this.mailSender.send(simpleMessages);
+	}
+}
